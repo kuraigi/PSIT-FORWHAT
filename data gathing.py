0eb08c11fa506562c609c6dab97053a9e747378b
kuraigi <42972606+kuraigi@users.noreply.github.com>
2018-12-19 14:06:25 +0700

    Update data gathing.py

--- data gathing.py
+++ data gathing.py
@@ -1,18 +1,14 @@
-from itertools import islice
 from twitch import TwitchHelix
-import json
 import csv
 
 client = TwitchHelix(client_id='w2fwifqxprja6cvm1i251tyqbgd1ot')
 streams_iterator = client.get_streams(page_size=10)
 
 
-with open('test11.csv', 'w', newline='',encoding="utf-8") as csvfile:
+with open('filename.csv', 'w', newline='',encoding="utf-8") as csvfile:
     fieldnames = ['user_name','language', 'game_id', 'game_name', 'start_time(UTC Time)', 'view_count']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    lst = []
-    dct = {}
 
     for stream in streams_iterator:
          game_top = client.get_games(game_ids=[stream['game_id']])
@@ -20,5 +16,5 @@ with open('test11.csv', 'w', newline='',encoding="utf-8") as csvfile:
          for name in game_top:
               writer.writerow({'user_name':stream["user_name"],'language': stream["language"], 'game_id':stream["game_id"], 'game_name': name['name'], 'start_time(UTC Time)': stream["started_at"], 'view_count': stream["viewer_count"]})
               
-    print('done')
+   
 
